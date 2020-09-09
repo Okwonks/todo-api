5177e3b0a86933b3c7e17789e32efb6498ddb211
Albert Oketch <albert@paygoenergy.co>
2020-09-09 09:10:37 +0300

    Models: use ternary operator in index

--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, { ...config, define: { freezeTableName:true }});
-}
+const sequelize = config.use_env_variable
+    ? new Sequelize(process.env[config.use_env_variable], config)
+    : new Sequelize(config.database, config.username, config.password, { ...config, define: { freezeTableName:true }});
 
 fs
   .readdirSync(__dirname)
